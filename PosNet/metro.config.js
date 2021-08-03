60a4a28a118828433fa4510b401bb868e57f2bf6
Sabit56 <sabit.a.sirat@gmail.com>
2021-08-03 22:04:33 +0600

    Added moveNet successfully!

---
+++ PosNet/metro.config.js
@@ -0,0 +1,16 @@
+const blacklist = require('metro-config/src/defaults/blacklist');
+    module.exports = {
+        transformer: {
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: false,
+                },
+            }),
+        },
+        resolver: {
+            assetExts: ['bin', 'txt', 'jpg', 'png'],
+            sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
+            blacklistRE: blacklist([/platform_node/])
+        },
+    };
